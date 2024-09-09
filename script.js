eafb507fdc1f554b9c94f7c5202a7d54673cb536
Christina <cligare@hotmail.com>
2024-09-09 16:58:34 -0700

    add more javascript and styling

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const tipPerPerson = document.getElementById("tip-per-person");
 const tipTotal = document.getElementById("tip-total");
 const totalBill = document.getElementById("total-bill");
 const numberOfPeople = document.getElementById("number-of-people");
+const billErrorMsg = document.getElementById("bill-error-msg");
+const peopleErrorMsg = document.getElementById("people-error-msg");
+const resetBtn = document.getElementById("reset-btn");
 
-// console.log(tip);
-// console.log(`Button with ${tipPercentage} clicked!`);
+billInput.value = 0;
+numberOfPeople.value = 0;
 
 //Event Listener:  User clicks a tip button after entering a bill amount
 tipBtns.forEach((button) => {
@@ -16,39 +19,43 @@ tipBtns.forEach((button) => {
 
 function handleTip(e) {
   const button = e.target;
-  const tipPercentage = parseFloat(button.dataset.percent);
-  const people = parseFloat(numberOfPeople.value);
-  const billAmount = parseFloat(billInput.value);
+  const tipPercentage = Number(button.dataset.percent);
+  const people = Number(numberOfPeople.value);
+  const billAmount = Number(billInput.value);
+
+  //Validation
+  if (!billAmount) {
+    billErrorMsg.textContent = "Please enter the total bill amount";
+  } else {
+    billErrorMsg.textContent = "";
+  }
+
+  if (!people) {
+    peopleErrorMsg.textContent = "Can't be zero";
+  } else {
+    peopleErrorMsg.textContent = "";
+  }
 
   //Calculate tip and total
   const tip = (billAmount * tipPercentage).toFixed(2);
-  const total = (billAmount + parseFloat(tip)).toFixed(2);
+  const total = (billAmount + Number(tip)).toFixed(2);
   const tipPerPersonAmount = (tip / people).toFixed(2);
 
-  //If no entry or number of people is 1 or less
-  if (isNaN(people) || people <= 1) {
-    tipPerPerson.textContent = `$${tip}`;
-  } else {
-    tipPerPerson.textContent = `$${tipPerPersonAmount}`;
-  }
-
   //Updating UI
+  tipPerPerson.textContent = `$${tipPerPersonAmount}`;
   tipTotal.textContent = `$${tip}`;
   totalBill.textContent = `$${total}`;
 }
 
 // Or user creates a custom tip with pop-up entry
 
-//User also has to enter the number of people sharing the tip
+resetBtn.addEventListener("click", resetTipCalc);
 
-//Tip amount total displays
-
-//Total bill + tip displays
-
-//Make sure that the app can be reset with reset button
-
-//Edge cases
-
-//Error messages
-
-//If no people entered assume it is only one person
+//Reset button
+function resetTipCalc() {
+  billInput.value = "0";
+  numberOfPeople.value = "0";
+  tipPerPerson.textContent = "$0.00";
+  tipTotal.textContent = "$0.00";
+  totalBill.textContent = "$0.00";
+}
